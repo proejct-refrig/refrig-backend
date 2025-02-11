e4b4ffe3b19add7a8f5c48ca0adf119ec7cf5d69
simidot <youshin.dev@gmail.com>
2025-02-11 17:19:09 +0900

    first commit

---
+++ src/main/java/com/example/refrig/RefrigBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.refrig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefrigBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefrigBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/refrig/RefrigBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refrig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefrigBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
